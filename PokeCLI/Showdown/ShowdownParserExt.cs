7b6723bd3cb4d9348a082cdb071942a8d315bae4
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-05-14 00:37:46 +0200

    style: improve extension method style

--- PokeCLI/Showdown/ShowdownParserExt.cs
+++ PokeCLI/Showdown/ShowdownParserExt.cs
@@ -6,22 +6,23 @@ namespace PokeCLI.Showdown
     public static class ShowdownParserExt
     {
         /// <summary>
-        /// <inheritdoc cref="ParserRuleContext.GetText"/>
-        /// <para>It preserves whitespaces from hidden channel.</para>
+        ///     <inheritdoc cref="ParserRuleContext.GetText" />
+        ///     <para>It preserves whitespaces from hidden channel.</para>
         /// </summary>
         /// <remarks>
-        /// From <a href="https://stackoverflow.com/a/26533266/7907339">StackOverflow</a>, credit to Lucas Trzesniewski.
+        ///     From <a href="https://stackoverflow.com/a/26533266/7907339">StackOverflow</a>, credit to Lucas Trzesniewski.
         /// </remarks>
         /// <param name="context">the parser rule context to retrieve full text from</param>
         /// <returns>the parsed text including hidden channel (mainly spaces)</returns>
-        /// <seealso cref="RuleContext.GetText"/>
-        ///
+        /// <seealso cref="RuleContext.GetText" />
         public static string GetFullText(this ParserRuleContext context)
         {
-            if (context.Start == null || context.Stop == null || context.Start.StartIndex < 0 || context.Stop.StopIndex < 0)
-                return context.GetText(); // Fallback
-
-            return context.Start.InputStream.GetText(Interval.Of(context.Start.StartIndex, context.Stop.StopIndex));
+            return context.Start == null
+                   || context.Stop == null
+                   || context.Start.StartIndex < 0
+                   || context.Stop.StopIndex < 0
+                ? context.GetText() // Fallback
+                : context.Start.InputStream.GetText(Interval.Of(context.Start.StartIndex, context.Stop.StopIndex));
         }
     }
 }
